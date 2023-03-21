828f5821f51943fddd51d6206511b0c928b629bb
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2023-03-21 13:16:56 +0530

    Add files via upload

---
+++ android/when.kt
@@ -0,0 +1,36 @@
+fun main()
+{
+    var num=5
+
+    when(num)
+    {
+        1->
+        {
+            println("Monday")
+        }
+        2->
+        {
+            println("Tuesday")
+        }
+        3->
+        {
+            println("Wednesday")
+        }
+        4->
+        {
+            println("Thursday")
+        }
+        5->
+        {
+            println("Friday")
+        }
+        6->
+        {
+            println("Saturday")
+        }
+        7->
+        {
+            println("Sunday")
+        }
+    }
+}
