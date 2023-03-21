109f4c37cce7e4ebaaec12c9432e4be78d668591
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2023-03-21 12:41:07 +0530

    Add files via upload

---
+++ Task/Details.kt
@@ -0,0 +1,7 @@
+fun main()
+{
+    println("My Name is Rutvik Babariya ")
+    println("My Birth date is 24th march 2000 ")
+    println("I am 22 years old ")
+    println("I am from manavadar, junagadh")
+}
