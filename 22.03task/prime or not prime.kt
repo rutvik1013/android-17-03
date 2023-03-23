48aaf92c078df345a1df6b48743952ddc03599c9
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2023-03-23 22:08:17 +0530

    Update prime or not prime.kt

--- 22.03task/prime or not prime.kt
+++ 22.03task/prime or not prime.kt
@@ -2,13 +2,21 @@ fun main()
 {
     println("Enter Number:")
     var n=readLine()!!.toInt()
-
-    if(n%2==0)
+    var number=0
+    for(i in 2 until n)
     {
-        println("Prime Number$n")
+        if(n%i==0)
+        {
+            number=1
+            break
+        }
+    }
+    if(number==1)
+    {
+        println("Prime Number")
     }
     else
     {
-        println("Not Prime$n")
+        println("Not prime")
     }
 }
