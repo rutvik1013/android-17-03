678e5085ec84caee8b86a49a08bdbaf6c2b12386
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2023-03-23 14:50:09 +0530

    Add files via upload

---
+++ 22.03task/multiply floting point.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    var a=5.5
+    var b=6.2
+    var c='c'
+    var ascii=c.toInt()
+    var multi=a*b
+
+    println("Multiplication$multi")
+    println("ASCII value $c:$ascii")
+}
