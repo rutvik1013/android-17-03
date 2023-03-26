dd190721f49ab99b4c524625a871d4873e4dcbc8
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2023-03-26 21:28:45 +0530

    Add files via upload

---
+++ 22.03task/25.03 Task/pattern13.kt
@@ -0,0 +1,20 @@
+fun main()
+{
+    for(i in 1..5)
+    {
+        for(j in 1..i)
+        {
+            print("*")
+        }
+        println()
+
+    }
+    for(k in 4 downTo 1)
+    {
+        for(a in 1..k)
+        {
+            print("*")
+        }
+        println()
+    }
+}
